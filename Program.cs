b8f2136aed334bafe4e6a7d5921767bc53e9bbe6
Vlad Dulau <vlad.dulau@amdaris.com>
2023-05-25 11:26:02 +0300

    Add initial text

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+
+
+
+
+
+Console.WriteLine("Hello, World from main branch!");
+
+
+
+class Text
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    
+}
---
+++ TextConflict.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTutorial
+{
+    internal class TextConflict
+    {
+        public static string GreetingMessage()
+        {
+            return "Hello world source control";
+        }
+    }
+}
